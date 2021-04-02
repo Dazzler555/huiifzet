6644935b68fbf38e9ad497dfa8641a31acd1d00f
Dazzler555 <71560605+Dazzler555@users.noreply.github.com>
2021-04-02 19:04:50 +0530

    unnecessary

--- setup/android_build_env.sh
+++ setup/android_build_env.sh
@@ -45,21 +45,21 @@ sudo DEBIAN_FRONTEND=noninteractive \
     "${PACKAGES}" -y
 
 # For all those distro hoppers, lets setup your git credentials
-GIT_USERNAME="$(git config --get user.name)"
-GIT_EMAIL="$(git config --get user.email)"
-echo "Configuring git"
-if [[ -z ${GIT_USERNAME} ]]; then
-    echo -n "Enter your name: "
-    read -r NAME
-    git config --global user.name "${NAME}"
-fi
-if [[ -z ${GIT_EMAIL} ]]; then
-    echo -n "Enter your email: "
-    read -r EMAIL
-    git config --global user.email "${EMAIL}"
-fi
-git config --global credential.helper "cache --timeout=7200"
-echo "git identity setup successfully!"
+# GIT_USERNAME="$(git config --get user.name)"
+# GIT_EMAIL="$(git config --get user.email)"
+# echo "Configuring git"
+#if [[ -z ${GIT_USERNAME} ]]; then
+    #echo -n "Enter your name: "
+  #  read -r NAME
+ #   git config --global user.name "${NAME}"
+#fi
+#if [[ -z ${GIT_EMAIL} ]]; then
+#    echo -n "Enter your email: "
+    #read -r EMAIL
+  #  git config --global user.email "${EMAIL}"
+#fi
+#git config --global credential.helper "cache --timeout=7200"
+#echo "git identity setup successfully!"
 
 # From Ubuntu 18.10 onwards and Debian Buster libncurses5 package is not available, so we need to hack our way by symlinking required library
 # shellcheck disable=SC2076
